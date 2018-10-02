a874ca0b2c3ee3af99f5e97d3cfe79b10ccb8615
RB <antoniopanetrass@gmail.com>
2018-10-02 16:52:12 +0200

    changes on master branch

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,7 +2,8 @@ public class SomeProgram {
 	
 	public static void main(String[] args){
 		System.out.println("tekst jakis");
-		System.out.println("tekst2")
+		System.out.println("tekst2scs")
+		System.out.println("teksts")
 		
 	}
 	
